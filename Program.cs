f3055b19e06256a63d189551e4122f600e0a4a85
raditya <radityabagaswara@students.amikom.ac.id>
2021-03-18 12:41:54 +0700

    menambahkan program taxi class dan object

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.NamaPengemudi = "Gepeng";
+            taxi.DalamPerjalanan = true;
+            taxi.JumlahPenumpang = 10;
+
+            taxi.TaxiInfo();
+            taxi.MenjemputPenumpang();
+            taxi.SudahSampai();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Taxi
+{
+    class Taxi
+    {
+        public string NamaPengemudi { get; set; }
+        public bool DalamPerjalanan { get; set; }
+        public int JumlahPenumpang { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Nama Pengemudi : {0}", NamaPengemudi);
+            if(DalamPerjalanan == true)
+            {
+                Console.WriteLine("Dalam Perjalanan : Ya");
+            }
+            if(DalamPerjalanan == false)
+            {
+                Console.WriteLine("Dalam Perjalanan : Tidak");
+            }
+
+            Console.WriteLine("Jumlah Penumpang {0}", JumlahPenumpang);
+        }
+
+        public void MenjemputPenumpang()
+        {
+            Console.WriteLine("{0} Sedang menjemput penumpang", NamaPengemudi);
+        }
+
+        public void SudahSampai()
+        {
+            Console.WriteLine("{0} Sudah sampai tujuan penumpang", NamaPengemudi);
+        }
+    }
+}
